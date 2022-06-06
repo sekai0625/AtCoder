fb181e65ef818ca687dbb96683adbc3d6b0a75d9
sekai0625 <yusaku.takahama0625@gmail.com>
2022-06-07 01:03:27 +0900

    add B

--- ABC254/B.py
+++ ABC254/B.py
@@ -3,10 +3,15 @@ n = int(input())
 A = []
 for i in range(n):
     tmp = []
-    for j in range(i):
+    for j in range(i + 1):
         if j == 0 or i == j:
             tmp.append(1)
         else:
             A_tmp = A[i-1]
             tmp.append(A_tmp[j-1] + A_tmp[j])
     A.append(tmp)
+
+for a in A:
+    a = list(map(str, a))
+    line = ' '.join(a)
+    print(line)
