5bc02f84659cab514e44db896b7ae6b451754959
sekai0625 <yusaku.takahama0625@gmail.com>
2022-06-07 14:25:29 +0900

    C m

--- ABC254/C.py
+++ ABC254/C.py
@@ -1,20 +1,21 @@
-import random
+import copy
 
 n, k = map(int, input().split(' '))
 
-A = [random.randint(1, 10^9) for _ in range(n)]
-sorted_A = A
+A = list(map(int, input().split(' ')))
+sorted_A = copy.copy(A)
 sorted_A.sort()
-for i in range(n + 1):
-    if i + k <= n:
+for i in range(n):
+    if i + k < n:
         if A[i] > A[i + k]:
             A[i], A[i + k] = A[i + k], A[i]
 
 flag = 0
-for i in range(n + 1):
-    if i + k <= n:
-        if A[i] != sorted_A[i]:
+for i in range(n):
+    if A[i] != sorted_A[i]:
             flag = 1
+
+# print("{}\n{}".format(sorted_A, A))
 
 if flag == 0:
     print('Yes')
